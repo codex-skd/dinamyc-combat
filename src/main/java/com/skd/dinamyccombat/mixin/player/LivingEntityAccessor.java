3d2c83b5b788f6093197213932a8988d409d7f06
LlaGuiTo <santiago.losada.borrajo@gmail.com>
2026-07-07 02:55:49 +0200

    v1.0.0-beta.6: Fix LivingEntityAccessor crash - invalid invulnerableTime field

--- src/main/java/com/skd/dinamyccombat/mixin/player/LivingEntityAccessor.java
+++ src/main/java/com/skd/dinamyccombat/mixin/player/LivingEntityAccessor.java
@@ -3,25 +3,14 @@ package com.skd.dinamyccombat.mixin.player;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(LivingEntity.class)
 public interface LivingEntityAccessor {
 
     @Accessor("attackStrengthTicker")
-    int getAttackStrengthTicker();
+    int betterCombat_getTicksSinceLastAttack();
 
     @Accessor("attackStrengthTicker")
-    void setAttackStrengthTicker(int value);
-
-    @Accessor("invulnerableTime")
-    int getInvulnerableTime();
-
-    @Accessor("invulnerableTime")
-    void setInvulnerableTime(int value);
-
-    @Accessor("lastHurtByPlayerTime")
-    int getLastHurtByPlayerTime();
-
-    @Accessor("lastHurtByPlayerTime")
-    void setLastHurtByPlayerTime(int value);
+    void betterCombat_setTicksSinceLastAttack(int value);
 }

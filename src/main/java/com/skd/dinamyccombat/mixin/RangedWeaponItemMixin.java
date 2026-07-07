5cdf3da260e60f34c0ed2c286ce13b99672feab7
LlaGuiTo <santiago.losada.borrajo@gmail.com>
2026-07-07 02:51:03 +0200

    v1.0.0-beta.4: Fix LivingEntityMixin, EnchantmentMixin, RangedWeaponItemMixin, ServerPlayNetworkHandlerMixin

--- src/main/java/com/skd/dinamyccombat/mixin/RangedWeaponItemMixin.java
+++ src/main/java/com/skd/dinamyccombat/mixin/RangedWeaponItemMixin.java
@@ -1,32 +1,28 @@
 package com.skd.dinamyccombat.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.skd.dinamyccombat.logic.InventoryUtil;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ProjectileWeaponItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.function.Predicate;
 
 @Mixin(ProjectileWeaponItem.class)
 public class RangedWeaponItemMixin {
 
-    @Inject(method = "getHeldProjectile", at = @At("HEAD"), cancellable = true)
-    private static void dinamyc_combat$getHeldProjectile(LivingEntity entity,
-            Predicate<ItemStack> predicate, CallbackInfoReturnable<ItemStack> cir) {
-        ItemStack offhandStack = entity.getItemInHand(InteractionHand.OFF_HAND);
-        if (predicate.test(offhandStack)) {
-            ItemStack mainHandStack = entity.getItemInHand(InteractionHand.MAIN_HAND);
-            if (!isTwoHandedWeapon(mainHandStack)) {
-                cir.setReturnValue(offhandStack);
-            }
+    @WrapOperation(
+            method = "getProjectile",
+            require = 0,
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;")
+    )
+    private static ItemStack dinamyc_combat$getHeldProjectile(LivingEntity entity, InteractionHand hand, Operation<ItemStack> original) {
+        if (entity instanceof Player player) {
+            return InventoryUtil.getOffHandSlotStack(player);
         }
-    }
-
-    private static boolean isTwoHandedWeapon(ItemStack stack) {
-        return false;
+        return original.call(entity, hand);
     }
 }
